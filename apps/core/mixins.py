87a8f7a30c85db9f859528198df7595a04c21625
David Macario <david.macariopro@gmail.com>
2026-06-28 16:11:12 +0200

    create default model mixins

---
+++ apps/core/mixins.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from typing import override
+from django.db import models
+from django.utils import timezone
+
+
+class TimestampsMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        return self.update(deleted_at=timezone.now())
+
+    def hard_delete(self):
+        return super().delete()
+
+    def alive(self):
+        return self.filter(deleted_at__isnull=True)
+
+    def dead(self):
+        return self.filter(deleted_at__isnull=False)
+
+
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        # Automatically exclude soft-deleted records
+        return SoftDeleteQuerySet(self.model, using=self._db).alive()
+
+
+class SoftDeleteMixin(models.Model):
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    objects = SoftDeleteManager()
+    all_objects = models.Manager()  # Escape hatch to see everything
+
+    def delete(self, *args, **kwargs):
+        self.deleted_at = timezone.now()
+        self.save(update_fields=["deleted_at"])
+
+    def hard_delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+
+    class Meta:
+        abstract = True

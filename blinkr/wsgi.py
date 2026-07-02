49599b25a938cd3c5d016f3f4ab0fea7e9b7b97f
David Macario <david.macariopro@gmail.com>
2026-07-02 02:45:40 +0200

    feat(models): add v1 routes & views, format project

--- blinkr/wsgi.py
+++ blinkr/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blinkr.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blinkr.settings")
 
 application = get_wsgi_application()
